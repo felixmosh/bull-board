aa8f1050964ee2d05955a6f3b0438a39cc10158f
Vitor Capretz <capretzvitor@gmail.com>
2019-08-27 16:15:34 +0200

    ui: move css file outside components folder

--- ui/index.js
+++ ui/index.js
@@ -1,6 +1,6 @@
 import React from 'react'
 import { render } from 'react-dom'
-import './components/index.css'
+import './index.css'
 import App from './components/App'
 
 render(<App basePath={window.basePath} />, document.getElementById('root'))
