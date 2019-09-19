dee8ea63e84c1d294c4564bde839a9e9df317b09
Alex Ross <Alex.Ross@genesys.com>
2019-09-19 13:44:40 -0400

    Added clean all option?
    - I think Clean All works now

---
+++ routes/cleanAll.js
@@ -0,0 +1,23 @@
+module.exports = async function handler(req, res) {
+  try {
+    const { queueName } = req.params
+    const { queues } = req.app.locals
+
+    const GRACE_TIME_MS = 5000
+
+    const queue = queues[queueName]
+    if (!queue) {
+      return res.status(404).send({ error: 'queue not found' })
+    }
+
+    await queue.clean(GRACE_TIME_MS, 'delayed')
+
+    return res.sendStatus(200)
+  } catch (e) {
+    const body = {
+      error: 'queue error',
+      details: e.stack,
+    }
+    return res.status(500).send(body)
+  }
+}
