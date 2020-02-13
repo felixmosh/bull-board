280d7706899345c5a9d5c65eefd302c797065c45
Erik Engervall <erik.engervall@gmail.com>
2020-02-13 22:55:18 +0100

    introduce dockest

--- src/index.spec.ts
+++ src/index.spec.ts
@@ -1,3 +1,7 @@
+/* eslint-disable @typescript-eslint/camelcase */
+import { Queue } from 'bullmq'
+import request from 'supertest'
+
 import * as bullBoard from './index'
 
 describe('index', () => {
@@ -8,5 +12,65 @@ describe('index', () => {
         "setQueues": [Function],
       }
     `)
+  })
+})
+
+describe('happy', () => {
+  const { router, setQueues } = bullBoard
+
+  it('should be able to set queue', async () => {
+    const paintQueue = new Queue('Paint', {
+      connection: {
+        host: 'localhost',
+        port: 6379,
+      },
+    })
+
+    setQueues([paintQueue])
+
+    await request(router)
+      .get('/queues')
+      .expect('Content-Type', /json/)
+      .expect(200)
+      .then(res => {
+        expect(JSON.parse(res.text)).toMatchInlineSnapshot(
+          {
+            stats: {
+              blocked_clients: expect.any(String),
+              connected_clients: expect.any(String),
+              mem_fragmentation_ratio: expect.any(String),
+              redis_version: expect.any(String),
+              total_system_memory: expect.any(String),
+              used_memory: expect.any(String),
+            },
+          },
+          `
+          Object {
+            "queues": Array [
+              Object {
+                "counts": Object {
+                  "active": 0,
+                  "completed": 0,
+                  "delayed": 0,
+                  "failed": 0,
+                  "paused": 0,
+                  "waiting": 0,
+                },
+                "jobs": Array [],
+                "name": "bull:Paint:~",
+              },
+            ],
+            "stats": Object {
+              "blocked_clients": Any<String>,
+              "connected_clients": Any<String>,
+              "mem_fragmentation_ratio": Any<String>,
+              "redis_version": Any<String>,
+              "total_system_memory": Any<String>,
+              "used_memory": Any<String>,
+            },
+          }
+        `,
+        )
+      })
   })
 })
