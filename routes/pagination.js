145c8e273e419754f9cf3988abe2770b130c8caf
Shaung Cheng <shaungc@umich.edu>
2020-05-26 19:17:21 -0700

    complete backend pagination parsing

---
+++ routes/pagination.js
@@ -0,0 +1,16 @@
+export const getPaginationParameters = query => {
+  let start = 0
+  let end = 9
+  if (query.start && query.end) {
+    const parsedStart = parseInt(query.start)
+    const parsedEnd = parseInt(query.end)
+    if (parsedStart !== NaN) {
+      start = parsedStart
+    }
+    if (parsedEnd !== NaN) {
+      end = parsedEnd
+    }
+  }
+
+  return [start, end]
+}
