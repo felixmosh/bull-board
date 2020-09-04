1426c7fd2e1571e7de75c6a041835cf3b02249d4
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2020-09-04 13:47:12 +0300

    Improve ui build

--- webpack.config.js
+++ webpack.config.js
@@ -2,15 +2,22 @@
 
 const path = require('path')
 const webpack = require('webpack')
+const MiniCssExtractPlugin = require('mini-css-extract-plugin')
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const ForkTsCheckerWebpackPlugin = require('fork-ts-checker-webpack-plugin')
+
+const basePath = '<%= basePath %>'
+const isProd = process.env.NODE_ENV === 'production'
 
 module.exports = {
-  mode: 'production',
+  mode: 'development',
   bail: true,
-  devtool: false,
+  devtool: isProd ? false : 'eval-cheap-module-source-map',
   entry: ['./src/ui/index.tsx'],
   output: {
     path: path.resolve(__dirname, './static'),
-    filename: 'bundle.js',
+    filename: `bundle${isProd ? '.[contenthash]' : ''}.js`,
+    publicPath: `${basePath}/static/`,
   },
   resolve: {
     extensions: ['.ts', '.tsx', '.js', '.jsx'],
@@ -24,25 +31,67 @@ module.exports = {
       },
       {
         test: /\.css$/,
-        use: ['style-loader', 'css-loader'],
+        use: [
+          process.env.NODE_ENV !== 'production'
+            ? 'style-loader'
+            : MiniCssExtractPlugin.loader,
+          'css-loader',
+        ],
       },
       {
         test: /\.ts(x?)$/,
         exclude: /node_modules/,
-        use: [{ loader: 'ts-loader' }],
-      },
-      {
-        // All output '.js' files will have any sourcemaps re-processed by 'source-map-loader'.
-        enforce: 'pre',
-        test: /\.js$/,
-        loader: 'source-map-loader',
+        use: [
+          {
+            loader: 'ts-loader',
+            options: {
+              transpileOnly: true,
+              compilerOptions: {
+                sourceMap: !isProd,
+              },
+            },
+          },
+        ],
       },
     ],
   },
   plugins: [
+    new MiniCssExtractPlugin({
+      filename: '[name].[contenthash].css',
+    }),
+    new HtmlWebpackPlugin({
+      filename: path.resolve(__dirname, './dist/ui/index.ejs'),
+      template: './src/ui/index.ejs',
+      templateParameters: {
+        basePath,
+      },
+    }),
     new webpack.ContextReplacementPlugin(
       /highlight.js\/lib\/languages$/,
       /^.\/(json|javascript)$/,
     ),
+    new ForkTsCheckerWebpackPlugin(),
   ],
+  optimization: {
+    splitChunks: {
+      chunks: 'all',
+      minSize: 20000,
+      maxSize: 0,
+      minChunks: 1,
+      maxAsyncRequests: 30,
+      maxInitialRequests: 30,
+      automaticNameDelimiter: '~',
+      cacheGroups: {
+        defaultVendors: {
+          test: /[\\/]node_modules[\\/]/,
+          priority: -10,
+        },
+        default: {
+          minChunks: 2,
+          priority: -20,
+          reuseExistingChunk: true,
+        },
+      },
+    },
+  },
 }
