d175eda6395b80dad26dc11e903e2cb3cef4cefb
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2020-12-10 22:45:14 +0200

    Add auth example

---
+++ examples/with-auth/index.js
@@ -0,0 +1,118 @@
+const { setQueues, router, BullMQAdapter } = require('bull-board');
+const { Queue: QueueMQ, Worker, QueueScheduler } = require('bullmq');
+const session = require('express-session');
+const bodyParser = require('body-parser');
+const passport = require('passport');
+const LocalStrategy = require('passport-local').Strategy;
+const { ensureLoggedIn } = require('connect-ensure-login');
+
+const app = require('express')();
+
+// Configure the local strategy for use by Passport.
+//
+// The local strategy require a `verify` function which receives the credentials
+// (`username` and `password`) submitted by the user.  The function must verify
+// that the password is correct and then invoke `cb` with a user object, which
+// will be set at `req.user` in route handlers after authentication.
+passport.use(new LocalStrategy(
+  function (username, password, cb) {
+    if (username === 'bull' && password === 'board') {
+      return cb(null, { user: 'bull-board' });
+    }
+    return cb(null, false);
+  }));
+
+// Configure Passport authenticated session persistence.
+//
+// In order to restore authentication state across HTTP requests, Passport needs
+// to serialize users into and deserialize users out of the session.  The
+// typical implementation of this is as simple as supplying the user ID when
+// serializing, and querying the user record by ID from the database when
+// deserializing.
+passport.serializeUser((user, cb) => {
+  cb(null, user);
+});
+
+passport.deserializeUser((user, cb) => {
+  cb(null, user);
+});
+
+// Configure view engine to render EJS templates.
+app.set('views', __dirname + '/views');
+app.set('view engine', 'ejs');
+
+const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t * 1000));
+
+const redisOptions = {
+  port: 6379,
+  host: 'localhost',
+  password: '',
+  tls: false,
+};
+
+const createQueueMQ = (name) => new QueueMQ(name, { connection: redisOptions });
+
+const run = async () => {
+  const exampleBullMqName = 'ExampleBullMQ';
+  const exampleBullMq = createQueueMQ(exampleBullMqName);
+
+  setQueues([new BullMQAdapter(exampleBullMq)]);
+
+  const queueScheduler = new QueueScheduler(exampleBullMqName, {
+    connection: redisOptions,
+  });
+  await queueScheduler.waitUntilReady();
+
+  new Worker(exampleBullMqName, async (job) => {
+    for (let i = 0; i <= 100; i++) {
+      await sleep(Math.random());
+      await job.updateProgress(i);
+
+      if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
+
+      return { jobId: `This is the return value of job (${job.id})` };
+    }
+  });
+
+  app.use(session({ secret: 'keyboard cat' }));
+  app.use(bodyParser.urlencoded({ extended: false }));
+
+  // Initialize Passport and restore authentication state, if any, from the session.
+  app.use(passport.initialize({}));
+  app.use(passport.session({}));
+
+  app.get('/ui/login',
+    (req, res) => {
+      res.render('login');
+    });
+
+  app.post('/ui/login',
+    passport.authenticate('local', { failureRedirect: '/ui/login' }),
+    (req, res) => {
+      res.redirect('/ui');
+    });
+
+  app.use('/add', (req, res) => {
+    const opts = req.query.opts || {};
+
+    exampleBullMq.add('Add', { title: req.query.title }, opts);
+
+    res.json({
+      ok: true,
+    });
+  });
+
+  app.use('/ui', ensureLoggedIn({ redirectTo: '/ui/login' }), router);
+
+  app.listen(3000, () => {
+    console.log('Running on 3000...');
+    console.log('For the UI, open http://localhost:3000/ui');
+    console.log('Make sure Redis is running on port 6379 by default');
+    console.log('To populate the queue, run:');
+    console.log('  curl http://localhost:3000/add?title=Example');
+    console.log('To populate the queue with custom options (opts), run:');
+    console.log('  curl http://localhost:3000/add?title=Test&opts[delay]=900');
+  });
+};
+
+run();
