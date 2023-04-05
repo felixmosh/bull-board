57c331bdbafb8063b4de53b6ca8db92e38c391ea
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-04-06 00:06:46 +0300

    Bump Hapi with auth example

--- examples/with-hapi-auth/index.js
+++ examples/with-hapi-auth/index.js
@@ -1,7 +1,7 @@
 const { createBullBoard } = require('@bull-board/api');
 const { BullMQAdapter } = require('@bull-board/api/bullMQAdapter');
 const { HapiAdapter } = require('@bull-board/hapi');
-const { Queue: QueueMQ, Worker, QueueScheduler } = require('bullmq');
+const { Queue: QueueMQ, Worker } = require('bullmq');
 const Hapi = require('@hapi/hapi');
 
 const sleep = (t) => new Promise((resolve) => setTimeout(resolve, t * 1000));
@@ -16,22 +16,21 @@ const redisOptions = {
 const createQueueMQ = (name) => new QueueMQ(name, { connection: redisOptions });
 
 async function setupBullMQProcessor(queueName) {
-  const queueScheduler = new QueueScheduler(queueName, {
-    connection: redisOptions,
-  });
-  await queueScheduler.waitUntilReady();
+  new Worker(
+    queueName,
+    async (job) => {
+      for (let i = 0; i <= 100; i++) {
+        await sleep(Math.random());
+        await job.updateProgress(i);
+        await job.log(`Processing job at interval ${i}`);
 
-  new Worker(queueName, async (job) => {
-    for (let i = 0; i <= 100; i++) {
-      await sleep(Math.random());
-      await job.updateProgress(i);
-      await job.log(`Processing job at interval ${i}`);
+        if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
+      }
 
-      if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
-    }
-
-    return { jobId: `This is the return value of job (${job.id})` };
-  });
+      return { jobId: `This is the return value of job (${job.id})` };
+    },
+    { connection: redisOptions }
+  );
 }
 
 async function validate(request, username, password) {
