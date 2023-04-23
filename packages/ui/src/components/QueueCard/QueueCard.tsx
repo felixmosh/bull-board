945d35ad6adf69aa8fa247e3198d2f6d43bc876d
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-04-23 21:58:44 +0300

    feat: make queue title as a link

--- packages/ui/src/components/QueueCard/QueueCard.tsx
+++ packages/ui/src/components/QueueCard/QueueCard.tsx
@@ -1,5 +1,6 @@
 import { AppQueue } from '@bull-board/api/dist/typings/app';
 import React from 'react';
+import { NavLink } from 'react-router-dom';
 import { Card } from '../Card/Card';
 import { QueueStats } from './QueueStats/QueueStats';
 import s from './QueueCard.module.css';
@@ -10,7 +11,11 @@ interface IQueueCardProps {
 
 export const QueueCard = ({ queue }: IQueueCardProps) => (
   <Card className={s.queueCard}>
-    <div>{queue.name}</div>
+    <div>
+      <NavLink to={`/queue/${encodeURIComponent(queue.name)}`} className={s.link}>
+        {queue.name}
+      </NavLink>
+    </div>
     <QueueStats queue={queue} />
   </Card>
 );
