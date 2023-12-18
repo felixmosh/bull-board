262ad477063ab7f83c9d1695a3e149080980ee0f
Felix Mosheev <9304194+felixmosh@users.noreply.github.com>
2023-12-18 16:58:43 +0200

    fix: remove old usage of QueueScheduler

--- examples/with-fastify-auth/index.js
+++ examples/with-fastify-auth/index.js
@@ -1,4 +1,4 @@
-const { Queue: QueueMQ, Worker, QueueScheduler } = require('bullmq');
+const { Queue: QueueMQ, Worker } = require('bullmq');
 const fastify = require('fastify');
 const { basicAuth } = require('./basicAuth');
 const { cookieAuth } = require('./cookieAuth');
@@ -15,24 +15,23 @@ const redisOptions = {
 const createQueueMQ = (name) => new QueueMQ(name, { connection: redisOptions });
 
 async function setupBullMQProcessor(queueName) {
-  const queueScheduler = new QueueScheduler(queueName, {
-    connection: redisOptions,
-  });
-  await queueScheduler.waitUntilReady();
+  new Worker(
+    queueName,
+    async (job) => {
+      for (let i = 0; i <= 100; i++) {
+        await sleep(Math.random());
+        await job.updateProgress(i);
+        await job.log(`Processing job at interval ${i}`);
 
-  new Worker(queueName, async (job) => {
-    for (let i = 0; i <= 100; i++) {
-      await sleep(Math.random());
-      await job.updateProgress(i);
-      await job.log(`Processing job at interval ${i}`);
+        if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
+      }
 
-      if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
+      return { jobId: `This is the return value of job (${job.id})` };
+    },
+    {
+      connection: redisOptions,
     }
-
-    return { jobId: `This is the return value of job (${job.id})` };
-  }, {
-    connection: redisOptions,
-  });
+  );
 }
 
 const run = async () => {
