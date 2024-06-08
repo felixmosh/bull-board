73c1230ea03e64c8b1257112b2a5a66dba3b55fe
Kravets <57632712+kravetsone@users.noreply.github.com>
2024-06-08 23:59:39 +0300

    feat: add elysia adapter

---
+++ examples/with-elysia/index.ts
@@ -0,0 +1,67 @@
+import { createBullBoard } from "@bull-board/api";
+import { BullMQAdapter } from "@bull-board/api/bullMQAdapter";
+import { ElysiaAdapter } from "@bull-board/elysia";
+import { Queue as QueueMQ, Worker } from "bullmq";
+import Elysia from "elysia";
+
+const sleep = (t: number) =>
+	new Promise((resolve) => setTimeout(resolve, t * 1000));
+
+const redisOptions = {
+	port: 6379,
+	host: "localhost",
+	password: "",
+	tls: false,
+};
+
+const createQueueMQ = (name: string) =>
+	new QueueMQ(name, { connection: redisOptions });
+
+async function setupBullMQProcessor(queueName: string) {
+	new Worker(
+		queueName,
+		async (job) => {
+			for (let i = 0; i <= 100; i++) {
+				await sleep(Math.random());
+				await job.updateProgress(i);
+				await job.log(`Processing job at interval ${i}`);
+
+				if (Math.random() * 200 < 1) throw new Error(`Random error ${i}`);
+			}
+
+			return { jobId: `This is the return value of job (${job.id})` };
+		},
+		{ connection: redisOptions },
+	);
+}
+
+const exampleBullMq = createQueueMQ("BullMQ");
+
+await setupBullMQProcessor(exampleBullMq.name);
+
+const app = new Elysia();
+
+const serverAdapter = new ElysiaAdapter("/ui");
+
+createBullBoard({
+	queues: [new BullMQAdapter(exampleBullMq)],
+	serverAdapter,
+});
+
+app.use(await serverAdapter.registerPlugin());
+
+app.get("/add", async ({ query }) => {
+	await exampleBullMq.add("Add", { title: query.title });
+
+	return { ok: true };
+});
+
+app.listen(3000, ({ port, url }) => {
+	/* eslint-disable no-console */
+	console.log(`Running on ${url.hostname}:${port}...`);
+	console.log(`For the UI of instance1, open http://localhost:${port}/ui`);
+	console.log("Make sure Redis is running on port 6379 by default");
+	console.log("To populate the queue, run:");
+	console.log(`  curl http://localhost:${port}/add?title=Example`);
+	/* eslint-enable no-console */
+});
---
+++ packages/elysia/src/ElysiaAdapter.ts
@@ -0,0 +1,180 @@
+import type {
+	AppControllerRoute,
+	AppViewRoute,
+	BullBoardQueues,
+	ControllerHandlerReturnType,
+	HTTPMethod,
+	IServerAdapter,
+	UIConfig,
+} from "@bull-board/api/typings/app";
+import staticPlugin from "@elysiajs/static";
+import ejs from "ejs";
+import { Elysia } from "elysia";
+
+export class ElysiaAdapter implements IServerAdapter {
+	private plugin: Elysia<any, any, any>;
+	private basePath = "";
+
+	constructor(basePath = "") {
+		this.basePath = basePath;
+		this.plugin = new Elysia({
+			name: "@bull-board/elysia",
+			prefix: basePath,
+		});
+	}
+
+	private entryRoute: AppViewRoute | undefined;
+	private statics: { path: string; route: string } | undefined;
+	private bullBoardQueues: BullBoardQueues | undefined;
+	private viewPath: string | undefined;
+	private uiConfig: UIConfig = {};
+
+	public setStaticPath(
+		staticsRoute: string,
+		staticsPath: string,
+	): ElysiaAdapter {
+		this.statics = { route: staticsRoute, path: staticsPath };
+
+		return this;
+	}
+
+	public setViewsPath(viewPath: string): ElysiaAdapter {
+		this.viewPath = viewPath;
+
+		return this;
+	}
+
+	public setErrorHandler(
+		handler: (error: Error) => ControllerHandlerReturnType,
+	) {
+		this.plugin.onError(({ error, set }) => {
+			const response = handler(error);
+			set.status = response.status || 500;
+
+			return response.body;
+		});
+
+		return this;
+	}
+
+	public setApiRoutes(routes: AppControllerRoute[]): ElysiaAdapter {
+		for (const { route, handler, method } of routes) {
+			const methods = Array.isArray(method) ? method : [method];
+
+			for (const method of methods) {
+				this.registerRoute(route, method, handler);
+			}
+		}
+
+		return this;
+	}
+
+	public setEntryRoute(routeDef: AppViewRoute): ElysiaAdapter {
+		this.entryRoute = routeDef;
+
+		return this;
+	}
+
+	public setQueues(bullBoardQueues: BullBoardQueues): ElysiaAdapter {
+		this.bullBoardQueues = bullBoardQueues;
+		return this;
+	}
+
+	public setUIConfig(config: UIConfig = {}): ElysiaAdapter {
+		this.uiConfig = config;
+
+		return this;
+	}
+
+	public async registerPlugin() {
+		if (!this.statics) {
+			throw new Error(
+				`Please call 'setStaticPath' before using 'registerHandlers'`,
+			);
+		}
+		if (!this.entryRoute) {
+			throw new Error(
+				`Please call 'setEntryRoute' before using 'registerHandlers'`,
+			);
+		}
+		if (!this.viewPath) {
+			throw new Error(
+				`Please call 'setViewsPath' before using 'registerHandlers'`,
+			);
+		}
+
+		if (!this.uiConfig) {
+			throw new Error(
+				`Please call 'setUIConfig' before using 'registerHandlers'`,
+			);
+		}
+
+		const { method, route, handler } = this.entryRoute;
+
+		const routes = Array.isArray(route) ? route : [route];
+
+		for (const route of routes) {
+			this.plugin.route(method.toUpperCase(), route, async () => {
+				const { name: filename, params } = handler({
+					basePath: this.basePath,
+					uiConfig: this.uiConfig,
+				});
+
+				return new Response(
+					await ejs.renderFile(`${this.viewPath}/${filename}`, params),
+					{
+						headers: {
+							"content-type": "text/html",
+						},
+					},
+				);
+			});
+		}
+
+		this.plugin.use(
+			await staticPlugin({
+				prefix: this.statics.route,
+				assets: this.statics.path,
+			}),
+		);
+
+		return this.plugin;
+	}
+
+	private registerRoute(
+		routeOrRoutes: string | string[],
+		method: HTTPMethod,
+		handler: AppControllerRoute["handler"],
+	) {
+		const { bullBoardQueues } = this;
+
+		if (!bullBoardQueues) {
+			throw new Error(
+				`Please call 'setQueues' before using 'registerHandlers'`,
+			);
+		}
+
+		const routes = Array.isArray(routeOrRoutes)
+			? routeOrRoutes
+			: [routeOrRoutes];
+
+		for (const route of routes) {
+			this.plugin.route(
+				method.toUpperCase(),
+				route,
+				async ({ params, body, query, set }) => {
+					const response = await handler({
+						queues: this.bullBoardQueues as BullBoardQueues,
+						params,
+						body: body as Record<string, unknown>,
+						query,
+					});
+
+					set.status = response.status || 200;
+
+					return response.body;
+				},
+			);
+		}
+	}
+}
