320515bfde6af81694c8d1995f6d74db4467045f
Henry Stelle <61335118+henryStelle@users.noreply.github.com>
2024-06-15 12:32:34 -0400

    fix(ui): correctly calculate the circumference and offset of the progress citcle (#772)
    * Fix progress circle offset
    
    * Remove rounded border
    
    * Fix firefox support

--- packages/ui/src/components/JobCard/Progress/Progress.tsx
+++ packages/ui/src/components/JobCard/Progress/Progress.tsx
@@ -4,39 +4,34 @@ import cn from 'clsx';
 import { Status } from '@bull-board/api/typings/app';
 import { STATUSES } from '@bull-board/api/src/constants/statuses';
 
-export const Progress = ({
-  percentage,
-  status,
-  className,
-}: {
+const radius = 65;
+const circumference = 2 * Math.PI * radius;
+
+interface ProgressProps {
   percentage: number;
   status: Status;
   className?: string;
-}) => (
-  <svg className={cn(s.progress, className)} viewBox="0 0 140 140">
-    <circle
-      cx="70"
-      cy="70"
-      r="65"
-      fill="none"
-      stroke="#E5E7EB"
-      strokeWidth="8"
-      strokeLinecap="round"
-    ></circle>
-    <circle
-      cx="70"
-      cy="70"
-      r="65"
-      fill="none"
-      stroke={status === STATUSES.failed ? '#F56565' : '#48BB78'}
-      strokeWidth="8"
-      strokeLinecap="round"
-      strokeDasharray="600"
-      strokeDashoffset={600 - ((600 - 160) * percentage) / 100}
-      style={{ transform: 'rotate(-90deg)' }}
-    ></circle>
-    <text textAnchor="middle" x="74" y="88">
-      {Math.round(percentage)}%
-    </text>
-  </svg>
-);
+}
+
+export const Progress = ({ percentage, status, className }: ProgressProps) => {
+  const circleProps = {
+    cx: 70,
+    cy: 70,
+    r: radius,
+  };
+  return (
+    <svg className={cn(s.progress, className)} viewBox="0 0 140 140">
+      <circle stroke="#E5E7EB" {...circleProps} />
+      <circle
+        stroke={status === STATUSES.failed ? '#F56565' : '#48BB78'}
+        strokeDasharray={circumference}
+        strokeDashoffset={circumference - (circumference * percentage) / 100}
+        style={{ transform: 'rotate(-90deg)' }}
+        {...circleProps}
+      />
+      <text textAnchor="middle" x="74" y="88">
+        {Math.round(percentage)}%
+      </text>
+    </svg>
+  );
+};
