416ed2960e55764b391cbd12505041a80faa13b7
Arnaud Valle <arnaud.valle@gmail.com>
2024-10-21 12:45:11 +0200

    feat: adjust styles for status tabs on overview page

--- packages/ui/src/components/StatusLegend/StatusLegend.tsx
+++ packages/ui/src/components/StatusLegend/StatusLegend.tsx
@@ -1,26 +1,27 @@
 import React from 'react';
 import { queueStatsStatusList } from '../../constants/queue-stats-status';
 import s from './StatusLegend.module.css';
-import { NavLink } from 'react-router-dom';
+import { NavLink, useLocation } from 'react-router-dom';
 import { useTranslation } from 'react-i18next';
 import { toCamelCase } from '../../utils/toCamelCase';
+import cn from 'clsx';
 
 export const StatusLegend = () => {
   const { t } = useTranslation();
+  const { search } = useLocation();
 
   return (
     <ul className={s.legend}>
       {queueStatsStatusList.map((status) => {
         const displayStatus = t(`QUEUE.STATUS.${status.toUpperCase()}`).toLocaleUpperCase();
 
-        return (<li key={status} className={s[toCamelCase(status)]}>
+        const query = new URLSearchParams(search);
+
+        return (<li key={status} className={cn(s[toCamelCase(status)], {
+          [s.isSelected]: query.get('status') === status,
+        })}>
           <NavLink
             to={`/?status=${status}`}
-            activeClassName={s.active}
-            isActive={(_path, { search }) => {
-              const query = new URLSearchParams(search);
-              return query.get('status') === status;
-            }}
             className={s[toCamelCase(status)]}
             key={`overview-${status}`}
           >
