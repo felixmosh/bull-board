c23220db8da5b9fde970c211f5ab3dcf167d4b14
Arnaud Valle <arnaud.valle@gmail.com>
2024-10-21 10:36:33 +0200

    feat: use status legend as filters

--- packages/ui/src/components/StatusLegend/StatusLegend.tsx
+++ packages/ui/src/components/StatusLegend/StatusLegend.tsx
@@ -1,18 +1,33 @@
 import React from 'react';
-import { useTranslation } from 'react-i18next';
 import { queueStatsStatusList } from '../../constants/queue-stats-status';
-import { toCamelCase } from '../../utils/toCamelCase';
 import s from './StatusLegend.module.css';
+import { NavLink } from 'react-router-dom';
+import { useTranslation } from 'react-i18next';
+import { toCamelCase } from '../../utils/toCamelCase';
 
 export const StatusLegend = () => {
   const { t } = useTranslation();
+
   return (
     <ul className={s.legend}>
-      {queueStatsStatusList.map((status) => (
-        <li key={status} className={s[toCamelCase(status)]}>
-          {t(`QUEUE.STATUS.${status.toUpperCase()}`)}
-        </li>
-      ))}
+      {queueStatsStatusList.map((status) => {
+        const displayStatus = t(`QUEUE.STATUS.${status.toUpperCase()}`).toLocaleUpperCase();
+
+        return (<li key={status} className={s[toCamelCase(status)]}>
+          <NavLink
+            to={`/?status=${status}`}
+            activeClassName={s.active}
+            isActive={(_path, { search }) => {
+              const query = new URLSearchParams(search);
+              return query.get('status') === status;
+            }}
+            className={s[toCamelCase(status)]}
+            key={`overview-${status}`}
+          >
+            <span title={displayStatus}>{displayStatus}</span>
+          </NavLink>
+        </li>);
+      })}
     </ul>
   );
 };
