46c2cf719fe141bc8b4d105349a26b96e5ad423a
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2025-01-02 12:15:51 +1300

    refactor(example.ts): update queue names and add delimiter support (hierarchy support)

--- example.ts
+++ example.ts
@@ -53,7 +53,7 @@ const run = async () => {
   const app = express();
 
   const exampleBull = createQueue3('ExampleBull');
-  const exampleBullMq = createQueueMQ('ExampleBullMQ');
+  const exampleBullMq = createQueueMQ('Examples.BullMQ');
   const flow = new FlowProducer({ connection: redisOptions });
 
   setupBullProcessor(exampleBull); // needed only for example proposes
@@ -135,7 +135,10 @@ const run = async () => {
   serverAdapter.setBasePath('/ui');
 
   createBullBoard({
-    queues: [new BullMQAdapter(exampleBullMq), new BullAdapter(exampleBull)],
+    queues: [
+      new BullMQAdapter(exampleBullMq, { delimiter: '.' }),
+      new BullAdapter(exampleBull, { delimiter: '.' }),
+    ],
     serverAdapter,
   });
 
