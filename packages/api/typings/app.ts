35619c4a5349d232bed0309276c6a538102cdb93
ahmad anwar <7552088+AhmedAnwarHafez@users.noreply.github.com>
2025-01-05 08:47:10 +1300

    feat: a new interface that is used for getFlow

--- packages/api/typings/app.ts
+++ packages/api/typings/app.ts
@@ -73,6 +73,12 @@ export interface QueueJobJson {
   parentKey?: string;
 }
 
+export interface JobTreeNode {
+  id: string;
+  name: string;
+  children?: JobTreeNode[];
+}
+
 export interface QueueJobOptions {
   delay?: number;
   attempts?: number;
