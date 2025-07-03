13648e6a12950a6d30f90db967bbc4acbf8c8123
Jaroslaw Weber <jaroslawweber@gmail.com>
2025-07-03 23:21:08 +0900

    Other/resizable left panel (#972)
    * change menu width from ui config
    
    * fix error
    
    ---------
    
    Co-authored-by: Jaroslaw Weber <jaroslaw.weber@adriel.com>

--- packages/ui/src/hooks/useUIConfig.ts
+++ packages/ui/src/hooks/useUIConfig.ts
@@ -1,8 +1,16 @@
-import { UIConfig } from '@bull-board/api/typings/app';
-import React, { useContext } from 'react';
+import { UIConfig } from '@bull-board/api/dist/typings/app';
+import React, { useContext, useEffect } from 'react';
 
 export const UIConfigContext = React.createContext<UIConfig>(null as any);
 
 export function useUIConfig() {
-  return useContext(UIConfigContext);
+  const uiConfig = useContext(UIConfigContext);
+
+  useEffect(() => {
+    if (uiConfig?.menu?.width) {
+      document.documentElement.style.setProperty('--menu-width', uiConfig.menu.width);
+    }
+  }, [uiConfig?.menu?.width]);
+
+  return uiConfig;
 }
