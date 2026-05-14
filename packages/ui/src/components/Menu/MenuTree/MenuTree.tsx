bcc80ae516961df9afab95e2b66effd2292e2747
mwalkersigma <131814372+mwalkersigma@users.noreply.github.com>
2026-05-13 23:30:22 -0500

    Fix group nesting (#1197)
    * Update MenuTree component padding logic
    
    Fix padding style for nested menu levels.
    fixes 1196
    
    * Remove level-specific styles from MenuTree
    
    Removed level-specific padding for menu items.
    
    * Introduce --level variable for menu item styling
    
    Add CSS variable for menu item level indentation
    
    * Refactor MenuTree component's style prop
    
    Per suggestion from felixmosh

--- packages/ui/src/components/Menu/MenuTree/MenuTree.tsx
+++ packages/ui/src/components/Menu/MenuTree/MenuTree.tsx
@@ -36,7 +36,7 @@ export const MenuTree = ({
   }));
 
   return (
-    <ul className={cn(s.menu, level > 0 && s[`level-${level}`])}>
+    <ul className={cn(s.menu, level > 0 && s.level)} style={{ '--level': level } as React.CSSProperties}>
       {tree.children.map((node) => {
         const isLeafNode = !node.children.length;
         const displayName = node.name;
