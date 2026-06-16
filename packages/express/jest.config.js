39fe79bff9dc1c7969198913c0f30621b8570b45
Aleksander Stós <aleksander.stos@gmail.com>
2026-06-16 17:38:55 +0200

    test(express): add server adapter contract suite

---
+++ packages/express/jest.config.js
@@ -0,0 +1,10 @@
+const pkg = require('./package.json');
+const { defaults: tsJest } = require('ts-jest/presets');
+module.exports = {
+  displayName: pkg.name,
+  preset: 'ts-jest',
+  testEnvironment: 'node',
+  transform: { ...tsJest.transform },
+  testMatch: ['<rootDir>/tests/**/*.spec.ts'],
+  testTimeout: 30000,
+};
